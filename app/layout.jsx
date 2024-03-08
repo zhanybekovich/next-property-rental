bc4a2ffb98a5e863f33e1933d615d8f0de8edb9d
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-03-08 10:49:08 +0600

    Add footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "@/assets/styles/global.css";
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export const metadata = {
@@ -12,6 +13,7 @@ const MainLayout = ({ children }) => {
       <body>
         <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
